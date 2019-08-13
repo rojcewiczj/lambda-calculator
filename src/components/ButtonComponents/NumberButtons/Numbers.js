57b5dd0734f95a7ec247e8e7a401b707202589be
John Rojcewicz <52579412+rojcewiczj@users.noreply.github.com>
2019-08-13 14:12:18 -0500

    exported modules, importing numbers

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {numbers} from "./src/data.js"
 
 //import any components needed
 
@@ -6,6 +7,7 @@ import React from "react";
 
 const Numbers = () => {
   // STEP 2 - add the imported data to state
+  const [numberState, setNumberState] = useState(numbers);
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
